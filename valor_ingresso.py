c17dbfe6bdae0d522f56f1290c8e90de2ee49ffd
Ramon Durães <ramongduraes@gmail.com>
2021-08-18 12:08:37 -0300

    adicionei docstring no codigo

--- valor_ingresso.py
+++ valor_ingresso.py
@@ -1,4 +1,11 @@
 def valor_cinema_opcional(idade, carteirinha_estudante=False):
+    """
+    Código para calcular o valor de um ingresso dada a idade de uma pessoa e se ela possui carteirinha de estudante.
+
+    :param idade: inteiro que representa a idade da pessoa
+    :param carteirinha_estudante: booleano que indica se a pessoa tem ou não carteirinha de estudante
+    :return: inteiro que representa o valor do ingresso
+    """
     valor_ingresso = 15  # valor normal
     valor_crianca = 11  # para pessoas com menos de 10 anos
     valor_idoso = 10  # para pessoas com mais de 60 anos
